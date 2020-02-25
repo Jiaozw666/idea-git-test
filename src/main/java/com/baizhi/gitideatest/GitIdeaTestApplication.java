c18ef6f7e27af447e298932262d5dda1bfa340ba
jiaozw <Jiaozw666@163.com>
2020-02-25 17:30:52 +0800

    第一次添加代码

---
+++ src/main/java/com/baizhi/gitideatest/GitIdeaTestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.gitideatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaTestApplication.class, args);
+    }
+
+}
