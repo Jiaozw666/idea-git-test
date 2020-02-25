0d36a570cbe75914106a428b87f1ce8388b0d80e
jiaozw <Jiaozw666@163.com>
2020-02-25 18:46:37 +0800

    第三次修改代码

---
+++ src/test/java/com/baizhi/test2.java
@@ -0,0 +1,16 @@
+package com.baizhi;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class test2 {
+
+    @Test
+    public void test1(){
+        System.out.println("第二个测试文件");
+        System.out.println("克隆之后第一次修改");
+    }
+
+}
