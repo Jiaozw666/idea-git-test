b8fddca93f33d02beed19e5fec25811bcbbfc9f6
jiaozw <Jiaozw666@163.com>
2020-02-25 17:54:57 +0800

    第一次修改代码

---
+++ src/test/java/com/baizhi/GitIdeaTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/baizhi/test1.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("第一次修改");
+    }
+
+}
